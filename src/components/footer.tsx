89eda3c7f699dc99df091d9ae54c738d1e25375c
Simon <snsimonescobar@gmail.com>
2022-02-17 00:24:21 -0500

    footer commented

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return(
-    <div className="attribution">
-      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-      Coded by <a href="#">Simón Escobar</a>.
+    <div className="">
+      {/* Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+      Coded by <a href="#">Simón Escobar</a>. */}
     </div>
   )
 }
